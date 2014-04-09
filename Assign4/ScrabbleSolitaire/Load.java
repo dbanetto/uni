8fa12428c5f3df0f2758dd08d1bd99b455dff6a1
David Banrett <david@barnett.net.nz>
2014-04-10 11:43:53 +1200

    Last mintue changes

--- Assign4/ScrabbleSolitaire/Load.java
+++ Assign4/ScrabbleSolitaire/Load.java
@@ -24,29 +24,29 @@ public class Load
                 switch (command)
                 {
                     case("commit"):
-                        String[] commits = para.split(";");
-                        for (String tile : commits)
+                    String[] commits = para.split(";");
+                    for (String tile : commits)
+                    {
+                        String[] items = tile.split(",");
+                        if (items.length == 3)
                         {
-                            String[] items = tile.split(",");
-                            if (items.length == 3)
-                            {
-                                //0 - x pois
-                                //1 - y pos
-                                //2 - name of tile
-                                Tile t = scabble.getRack().pickup(items[2]);
-                                int x = Integer.parseInt(items[0]);
-                                int y = Integer.parseInt(items[1]);
-                                scabble.getBoard().place(t, x, y);
-                            }
+                            //0 - x pois
+                            //1 - y pos
+                            //2 - name of tile
+                            Tile t = scabble.getRack().pickup(items[2]);
+                            int x = Integer.parseInt(items[0]);
+                            int y = Integer.parseInt(items[1]);
+                            scabble.getBoard().place(t, x, y);
                         }
-                        scabble.commit();
-                        break;
+                    }
+                    scabble.commit();
+                    break;
                     case ("seed"):
-                        int seed = Integer.parseInt(para);
-                        scabble.restart( seed );
-                        break;
+                    int seed = Integer.parseInt(para);
+                    scabble.restart( seed );
+                    break;
                 }
-                
+
                 UI.println(line);
             }
         } catch(IOException ex)
@@ -54,5 +54,64 @@ public class Load
             UI.println(ex.toString());
         }
 
+    }
+
+    public static void PlayBack(String saveFile, ScrabbleSolitaire scabble , double delay)
+    {
+        try {
+            BufferedReader fs = new BufferedReader( new FileReader(saveFile));
+
+            String line = "";
+
+            Board b = scabble.getBoard();
+            Rack r = scabble.getRack();
+            Bag bag = scabble.getBag();
+            while ( ( line  = fs.readLine() ) != null )
+            {
+                if (line.split(":").length != 2){
+                    UI.println("Invaild line " + line);
+                    continue;
+                }
+                String command = line.split(":")[0];
+                String para = line.split(":")[1];
+                switch (command)
+                {
+                    case("commit"):
+                    String[] commits = para.split(";");
+                    for (String tile : commits)
+                    {
+                        String[] items = tile.split(",");
+                        if (items.length == 3)
+                        {
+                            //0 - x pois
+                            //1 - y pos
+                            //2 - name of tile
+                            Tile t = scabble.getRack().pickup(items[2]);
+                            int x = Integer.parseInt(items[0]);
+                            int y = Integer.parseInt(items[1]);
+                            scabble.getBoard().place(t, x, y);
+                        }
+                    }
+                    UI.sleep(1000);
+                    scabble.commit();
+                    break;
+                    case ("seed"):
+                    int seed = Integer.parseInt(para);
+                    scabble.restart( seed );
+                    break;
+                }
+
+                UI.println(line);
+            }
+            UI.sleep(delay);
+            
+            scabble.setBoard(b);
+            scabble.setRack(r);
+            scabble.setBag(bag);
+
+        } catch(IOException ex)
+        {
+            UI.println(ex.toString());
+        }
     }
 }
--- Assign4/ScrabbleSolitaire/ScrabbleSolitaire.java
+++ Assign4/ScrabbleSolitaire/ScrabbleSolitaire.java
@@ -13,49 +13,47 @@ import java.util.*;
 import java.io.*;
 import java.awt.Color;
 
-
 /** Lets a user play a solitaire game of Scrabble.
-    Scrabble is a word game in which players take turns at adding words to
-    a crossword by placing tiles on a board. Each tile has a letter and a score.
+Scrabble is a word game in which players take turns at adding words to
+a crossword by placing tiles on a board. Each tile has a letter and a score.
 
-    There are 102 tiles in the whole game, which start off in a bag.
+There are 102 tiles in the whole game, which start off in a bag.
 
-    The player has a rack which holds seven tiles.
-    The player can rearrange tiles on their rack while they try to construct words;
-    they can move  tiles from their rack onto the board to make a word, and can move them
-    around on the board or back to the rack, until they commit to their word.  
-    The rack is then refilled with random tiles from the bag to replace the ones they put out.
-    They can't move any tiles that have been committed.
- 
-    The user can drag tiles around with the mouse - "pressing" on top of a tile will pick it up
-    (unless the user is already "holding" a tile), and "releasing" on the rack or on an empty
-    space on the board will put the currently held tile at that place.  Attempting to place
-    the tile on a full rack will not succeed. Placing the tile on top of a tile on the rack which
-    has some space will push the tiles to the side to make space for it.
+The player has a rack which holds seven tiles.
+The player can rearrange tiles on their rack while they try to construct words;
+they can move  tiles from their rack onto the board to make a word, and can move them
+around on the board or back to the rack, until they commit to their word.  
+The rack is then refilled with random tiles from the bag to replace the ones they put out.
+They can't move any tiles that have been committed.
 
+The user can drag tiles around with the mouse - "pressing" on top of a tile will pick it up
+(unless the user is already "holding" a tile), and "releasing" on the rack or on an empty
+space on the board will put the currently held tile at that place.  Attempting to place
+the tile on a full rack will not succeed. Placing the tile on top of a tile on the rack which
+has some space will push the tiles to the side to make space for it.
 
-    PROGRAM DESIGN
-    Each tile has a name, and a score.  It also contains the name of the image file for drawing
-    the tile.  All the images are square, and have a size of 48 pixels.
+PROGRAM DESIGN
+Each tile has a name, and a score.  It also contains the name of the image file for drawing
+the tile.  All the images are square, and have a size of 48 pixels.
 
-    The program has two arrays and an ArrayList to keep track of the tiles:
-      bag:   an arrayList of the remaining tiles.
-      rack:  an array of up to 7 tiles. Nulls represent cells with no tile .
-      board: a 15x15 array of cells where the tiles can be placed.
+The program has two arrays and an ArrayList to keep track of the tiles:
+bag:   an arrayList of the remaining tiles.
+rack:  an array of up to 7 tiles. Nulls represent cells with no tile .
+board: a 15x15 array of cells where the tiles can be placed.
 
-    The game is displayed with the board at the top, and the rack underneath.
-    When the player has picked up a tile to move it, but hasn't placed it yet,
-    the tile is shown in the top left corner.
+The game is displayed with the board at the top, and the rack underneath.
+When the player has picked up a tile to move it, but hasn't placed it yet,
+the tile is shown in the top left corner.
 
-    There is a Restart button, which will restart the game with an empty board, a refilled bag,
-    and a rack with 7 tiles taken from the bag.
+There is a Restart button, which will restart the game with an empty board, a refilled bag,
+and a rack with 7 tiles taken from the bag.
 
-    There is a "Commit" button, which will commit all the tiles on the board that haven't yet been
-    committed, and will refill the rack from the bag (as long as there are tiles in the bag).
+There is a "Commit" button, which will commit all the tiles on the board that haven't yet been
+committed, and will refill the rack from the bag (as long as there are tiles in the bag).
 
  */
 
-public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
+public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener,  UISliderListener {
 
     // fields to hold the bag, rack and board, and the tile in the hand
     /*# YOUR CODE HERE */
@@ -63,11 +61,12 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
     private Rack rack;
     private Bag bag;
     private Logger logger;
-    
+    private double delay = 100; 
+
     private Tile hand = null;
     private int score = 0;
     private int lastscore = 0;
-    
+
     /**
      * Set up the GUI (buttons and mouse listener) and restart the game
      */
@@ -76,13 +75,19 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
         UI.setMouseListener(this);
         UI.addButton("Commit", this);
         UI.addButton("Reset", this);
-        
+
         UI.addButton("Load", this);
-        UI.addButton("save", this);
-        
+        UI.addButton("Replay", this);
+        UI.addButton("Save", this);
+        UI.addSlider("Playback Delay (ms)", 100, 1000, this);
         this.restart();
     }
 
+    public void sliderPerformed(String action , double amount)
+    {
+        delay = amount;
+    }
+    
     /**
      * User can drag a tile from the board or the rack to a space on the board or the rack.
      * The user can also drag a tile from the board or rack to a full place on the rack if the
@@ -95,10 +100,10 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
         {
             int index = rack.index(x, y);
             UI.println("Rack Index : " + index);
-            
+
             int[] indexb = this.board.rowCol(x , y);
             UI.println("Board Index : " + indexb[0] + "," + indexb[1]);
-            
+
             //Make sure it is a valid index
             if (index != -1)
             {   
@@ -108,7 +113,7 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
                 }
                 draw();
             }
-            
+
             if (indexb[0] != -1)
             {
                 //Swap with tile below
@@ -124,18 +129,18 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
                 }
                 this.draw();
             }
-            
+
         }
         if (action.equals("released") && this.hand != null)
         {
             int index = rack.index(x, y);
             UI.println("Rack Index : " + index);
-            
+
             int[] indexb = this.board.rowCol(x , y);
             UI.println("Board Index : " + indexb[0] + "," + indexb[1]);
             if (indexb[0] != -1)
             {
-                 if (this.hand != null)
+                if (this.hand != null)
                 {
                     if (this.board.place(this.hand, indexb[0], indexb[1]))
                     {
@@ -165,7 +170,6 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
         }
     }
 
-
     
     /**
      * Respond to the buttons by calling methods.
@@ -186,20 +190,31 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
                 Load.Load( file , this );
                 this.draw();
             }
+        } else if (button.equals("Replay"))
+        {
+            String file = UIFileChooser.open();
+            if (file != null)
+            {
+                Load.PlayBack( file , this , delay );
+                this.draw();
+            }
+        } else if (button.equals("Load"))
+        {
+            UI.println("Game is already passivily saved by the Game Logger");
         }
-    }
 
+    }
 
     /** Restart the game:
      * set the board to be empty,
      * set the rack to have no tiles
      * set the bag to be empty, and then read the file of tile names into the bag
      * (keeping track of bagCount)
-    */
+     */
     public void restart(){
         this.restart((int)(Math.random() * Integer.MAX_VALUE ));
     }
-    
+
     public void restart(int seed){
         /*# YOUR CODE HERE */
         this.board = new Board();
@@ -207,9 +222,13 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
         this.bag.load("tile-bag.txt");
         this.rack  = new Rack();
         this.rack.fill(this.bag);
-        
+
         this.logger = new Logger( "scrabble" + System.currentTimeMillis() + seed + ".log" , seed );
         this.logger.Seed(seed);
+
+        hand = null;
+        score = 0;
+        lastscore = 0;
         draw();
     }
 
@@ -230,50 +249,50 @@ public class ScrabbleSolitaire implements UIMouseListener, UIButtonListener{
         UI.setFontSize(16);
         //last score is the score for the last word score
         UI.drawString("Score : " + score + " (+" + this.lastscore + ")", 15, 650);
-        
+
         UI.repaintGraphics();
     }
-    
+
     public void commit()
     {
-                    if (this.board.validPlay())
-            {
-                
-                this.rack.fill(this.bag);
-                
-                this.lastscore = this.board.score();
-                this.score += this.lastscore;
-                this.board.commit(this.logger);
-                this.draw();
-            }
+        if (this.board.validPlay())
+        {
+
+            this.rack.fill(this.bag);
+
+            this.lastscore = this.board.score();
+            this.score += this.lastscore;
+            this.board.commit(this.logger);
+            this.draw();
+        }
     }
-    
+
     public void setBoard(Board inBoard)
     {
         this.board = inBoard;
     }
-    
-        public void setRack(Rack inRack)
+
+    public void setRack(Rack inRack)
     {
         this.rack = inRack;
     }
-    
-        public void setBag(Bag inBag)
+
+    public void setBag(Bag inBag)
     {
         this.bag = inBag;
     }
-    
-        public Board getBoard()
+
+    public Board getBoard()
     {
         return this.board;
     }
-    
-        public Rack getRack()
+
+    public Rack getRack()
     {
         return this.rack;
     }
-    
-        public Bag getBag()
+
+    public Bag getBag()
     {
         return this.bag;
     }
