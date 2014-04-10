33f5ea6f731d210a529a5f0c5710872f11c956f2
David Banrett <david@barnett.net.nz>
2014-04-11 09:00:43 +1200

    Addded Assignment 5

---
+++ Assign5/ImageProcessor/ImageProcessor.java
@@ -0,0 +1,40 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP112 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP 102 Assignment 9 
+ * Name:
+ * Usercode:
+ * ID:
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import javax.swing.JColorChooser;
+
+/** ImageProcessor allows the user to load, display, modify, and save an image in a number of ways.
+The program should include
+ - Load, commit, save. (Core)
+ - Brightness adjustment (Core)
+ - Horizontal flip and 90 degree rotation. (Core)
+ - Merge  (Core)
+ - Crop&Zoom  (Core)
+ - Blur (3x3 filter)  (Core)
+
+ - Rotate arbitrary angle (Completion)
+ - Pour (spread-fill)  (Completion)
+ - General Convolution Filter  (Completion)
+
+ - Red-eye detection and removal (Challenge)
+ - Filter brush (Challenge)
+ */
+public class ImageProcessor implements UIButtonListener, UIMouseListener,  UISliderListener, UITextFieldListener{
+    /*# YOUR CODE HERE */
+
+
+
+}
