8e770c81add646e881f2696c3565bd43b96caef4
David Barnett <david@barnett.net.nz>
2014-08-05 16:24:06 +1200

    Assignment 3 init

---
+++ Assign3/MoleculeRenderer/MoleculeRenderer.java
@@ -0,0 +1,162 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+
+/* Code for COMP103, Assignment 3
+ * Name: 
+ * Usercode: 
+ * ID: 
+ */
+
+import ecs100.*;
+import java.awt.Color;
+import java.util.*;
+import java.io.*;
+
+/** Program to render a molecule on the graphics pane from different possible
+ *  perspectives.
+ *  A molecule consists of a collection of atoms.
+ *  Each atom has a type (eg, Carbon, or Hydrogen, or Oxygen, ..),
+ *  and a three dimensional position in the molecule (x, y, z).
+ *
+ *  Each molecule is described in a file by a list of atoms and their positions.
+ *  The molecule is rendered by drawing a colored circle for each atom.
+ *  The size and color of each atom is determined by the type of the atom.
+ * 
+ *  To make sure that the nearest atoms appear in front of the furthest atoms,
+ *  the atoms must be rendered in order from the furthest away to the nearest.
+ *  Each viewing perspective imposes a different ordering on the atoms.
+ *
+ *  The description of the size and color for rendering the different types of
+ *  atoms is stored in the file "element-table.txt" which should be read and
+ *  stored in a Map.  When an atom is rendered, the type should be looked up in
+ *  the map to find the size and color to pass to the atom's render() method
+ * 
+ *  A molecule can be rendered from different perspectives, and the program
+ *  provides four buttons to control the perspective of the rendering:
+ *   "Front" renders the molecule from the front (perspective = 0 degrees)
+ *   "Back" renders the molecule from the back (perspective = 180 degrees)
+ *   "Left" renders the molecule from the left (perspective = -90 degrees)
+ *   "Right" renders the molecule from the right (perspective = 90 degrees)
+ *   "PanLeft" decreases the perspective of the rendering by 5 degrees,
+ *   "PanRight" increases the perspective of the rendering by 5 degrees,
+ */
+
+public class MoleculeRenderer implements UIButtonListener {
+
+    // Fields
+    // Map containing the size and color of each atom type.
+    private Map<String,Element> elementTable; 
+
+    // The collection of the atoms in the current molecule
+    private List<Atom> molecule;  
+
+    private double currentAngle = 0.0;    //current viewing angle (in degrees)
+
+    private double panStep = 5.0;
+  
+    // Constructor:
+   /** Set up the Graphical User Interface and read the file of element data of
+     *	each possible type of atom into a Map: where the type is the key
+     *  and an ElementInfo object is the value (containing size and color).
+     */
+    public MoleculeRenderer() {
+	UI.addButton("Read", this);
+	UI.addButton("FromFront", this);
+	/*# YOUR CODE HERE */
+	readElementTable();    //  Read the element table first
+    }
+
+
+    /** Respond to button presses.
+     *  Most of the presses will set the currentAngle and sort the list of molecules
+     *  using the appropriate comparator
+     */
+    public void buttonPerformed(String button) {
+	if (button.equals("Read")) {
+	    currentAngle = 0;
+	    String filename = UIFileChooser.open();
+	    readMoleculeFile(filename);
+	    Collections.sort(molecule, new BackToFrontComparator());
+	}
+	else if (button.equals("FromFront")) {  // set the angle and sort from back to front
+	    currentAngle = 0;
+	    Collections.sort(molecule, new BackToFrontComparator());
+	}
+	/*# YOUR CODE HERE */
+
+	// render the molecule according to the current ordering
+	render();
+    }
+
+
+    /** Reads the molecule data from a file containing one line for each atom in
+     *  the molecule.
+     *  Each line contains an atom type and the 3D coordinates of the atom.
+     *  For each atom, it constructs an Atom object,
+     *   and adds it to the List of Atoms in the molecule.
+     *  To get the color and the size of each atom, it has to look up the type
+     *   of the atom in the Map of elements.
+     */
+    public void readMoleculeFile(String fname) {
+	try {
+	    /*# YOUR CODE HERE */
+
+	catch(IOException ex) {
+	    UI.println("Reading molecule file " + fname + " failed");
+	}
+    }
+    
+    /** (Completion) Reads a file containing radius and color information about each type of
+     *	atom and stores the info in a Map, using the atom type as a key
+     */
+    private void readElementTable() {
+	UI.println("Reading the element table file ...");
+	try {
+	    /*# YOUR CODE HERE */
+	}
+	catch (IOException ex) {
+	    UI.println("Reading element table file FAILED");
+	}
+    }
+    
+    /** Render the molecule, according the the current ordering of Atoms in the List.
+     *  The Atom's render() method needs the current perspective angle 
+     */
+    public void render() {
+	UI.clearGraphics();
+	for(Atom atom : molecule) {
+	    atom.render(currentAngle);
+	}
+	UI.repaintGraphics();
+    }
+
+    // Private comparator classes
+    // You will need a comparator class for each different direction
+    // used in the buttonPerformed method.
+    //
+    // Each comparator class should be a Comparator of Atoms, and will define
+    // a compare method that compares two atoms.
+    // Each comparator should have a compare method.
+    // Most of the comparators do not need an explicit constructor and have no fields.
+    // However, the comparator for the pan methods may need a field and a constructor
+
+    /** Comparator that will order atoms from back to front */
+    private class BackToFrontComparator implements Comparator<Atom> {
+	/**
+	 * Uses the z coordinates of the two atoms
+	 * larger z means towards the back,
+	 * smaller z means towards the front
+	 * Returns
+	 *  negative if atom1 is more to the back than atom2, (
+	 *  0 if they are in the same plane,
+	 *  positive if atom1 is more to the front than atom2.
+	 */
+    /*# YOUR CODE HERE */
+    }
+
+    public static void main(String args[]) {
+	new MoleculeRenderer();
+    }
+}
---
+++ Assign3/Sokoban/Sokoban.java
@@ -0,0 +1,276 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+
+/* Code for COMP 103, Assignment 3
+ * Name:
+ * Usercode:
+ * ID:
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+
+/** Sokoban
+ */
+
+public class Sokoban implements UIButtonListener, UIKeyListener {
+
+    // Fields
+    private Square[][] squares;   // the array describing the current warehouse.
+    private int rows;
+    private int cols;
+
+    private Coord agentPos;
+    private String agentDirection = "left";
+
+    private final int maxLevels = 4;
+    private int level = 0;
+
+    private Map<Character,Square> squareMapping;  // character in file to square type
+    private Map<Square,String> imageMapping;    // square type to image of square
+    private Map<String,String> agentMapping;    // direction to image of worker
+    private Map<String,String> keyMapping;      // key string to direction
+
+    // Constructors
+    /** Construct a new Sokoban object
+     *  and set up the GUI
+     */
+    public Sokoban() {
+        UI.addButton("New Level", this);
+        UI.addButton("Restart", this);
+        UI.addButton("left", this);
+        UI.addButton("up", this);
+        UI.addButton("down", this);
+        UI.addButton("right", this);
+
+        UI.println("Put the boxes away.");
+        UI.println("You may use keys (wasd or ijkl) but click on the graphics pane first");
+        UI.setKeyListener(this);
+
+        initialiseMappings();
+        load();
+    }
+
+    /** Respond to button presses */
+    public void buttonPerformed(String button) {
+        if (button.equals("New Level")) {
+            level = (level+1)%maxLevels;
+            load();
+        }
+        else if (button.equals("Restart"))
+            load();
+        else 
+            doAction(button);
+    }
+
+    /** Respond to key actions */
+    public void keyPerformed(String key) {
+        doAction(keyMapping.get(key));
+    }
+
+    /** Move the agent in the specified direction, if possible.
+     *  If there is box in front of the agent and a space in front of the box,
+     *    then push the box.
+     *  Otherwise, if there is anything in front of the agent, do nothing.
+     */
+    public void doAction(String dir) {
+        if (dir==null) return;
+        agentDirection = dir;
+        Coord newP = agentPos.next(dir);  // where the agent will move to
+        Coord nextP = newP.next(dir);     // the place two steps over
+        if ( squares[newP.row][newP.col].hasBox() && squares[nextP.row][nextP.col].free() ) {
+            push(dir);
+        }
+        else if ( squares[newP.row][newP.col].free() ) {
+            move(dir);
+        }
+    }
+
+    /** Move the agent into the new position (guaranteed to be empty) */
+    public void move(String dir) {
+        drawSquare(agentPos);
+        agentPos = agentPos.next(dir);
+        drawAgent();
+        Trace.println("Move " + dir);
+        UI.repaintGraphics();
+    }
+
+    /** Push: Move the agent, pushing the box one step */
+    public void push(String dir) {
+        drawSquare(agentPos);
+        agentPos = agentPos.next(dir);
+        drawAgent();
+        Coord boxP = agentPos.next(dir);
+        squares[agentPos.row][agentPos.col] = squares[agentPos.row][agentPos.col].moveOff();
+        squares[boxP.row][boxP.col] = squares[boxP.row][boxP.col].moveOn();
+        drawSquare(boxP);
+        Trace.println("Push " + dir);
+        UI.repaintGraphics();
+    }
+   
+    /** Pull: (useful for undoing a push in the opposite direction)
+     *  move the agent in direction from dir,
+     *  pulling the box into the agent's old position
+     */
+    public void pull(String dir) {
+        String opDir = oppositeDirection(dir);
+        Coord boxP = agentPos.next(opDir);
+        squares[boxP.row][boxP.col] = squares[boxP.row][boxP.col].moveOff();
+        squares[agentPos.row][agentPos.col] = squares[agentPos.row][agentPos.col].moveOn();
+        drawSquare(boxP);
+        drawSquare(agentPos);
+        agentPos = agentPos.next(dir);
+        agentDirection = opDir;
+        drawAgent();
+        Trace.println("Pull " + dir);
+        UI.repaintGraphics();
+    }
+
+    /** Load a grid of squares (and agent position) from a file */
+    public void load() {
+        File f = new File("warehouse" + level + ".txt");
+        if (f.exists()) {
+            List<String> lines = new ArrayList<String>();
+            try {
+                Scanner sc = new Scanner(f);
+                while (sc.hasNext())
+                    lines.add(sc.nextLine());
+                sc.close();
+            }
+            catch(IOException e) {
+                Trace.println("File error " + e);
+            }
+
+            rows = lines.size();
+            cols = lines.get(0).length();
+
+            squares = new Square[rows][cols];
+
+            for(int row = 0; row < rows; row++) {
+                String line = lines.get(row);
+                for(int col = 0; col < cols; col++) {
+                    if (col>=line.length())
+                        squares[row][col] = Square.empty;
+                    else {
+                        char ch = line.charAt(col);
+                        if ( squareMapping.containsKey(ch) )
+                            squares[row][col] = squareMapping.get(ch);
+                        else {
+                            squares[row][col] = Square.empty;
+                            UI.printf("Invalid char: (%d, %d) = %c \n",
+                                      row, col, ch);
+                        }
+                        if (ch=='A')
+                            agentPos = new Coord(row,col);
+                    }
+                }
+            }
+            draw();
+        }
+    }
+
+    // Drawing 
+
+    private static final int leftMargin = 40;
+    private static final int topMargin = 40;
+    private static final int squareSize = 25;
+
+    /** Draw the grid of squares on the screen, and the agent */
+    public void draw() {
+        UI.clearGraphics();
+        // draw squares
+        for(int row = 0; row<rows; row++)
+            for(int col = 0; col<cols; col++)
+                drawSquare(row, col);
+        drawAgent();
+        UI.repaintGraphics();
+    }
+
+    private void drawAgent() {
+        UI.drawImage(agentMapping.get(agentDirection),
+                     leftMargin+(squareSize* agentPos.col),
+                     topMargin+(squareSize* agentPos.row),
+                     squareSize, squareSize, false);
+    }
+
+    private void drawSquare(Coord pos) {
+        drawSquare(pos.row, pos.col);
+    }
+
+    private void drawSquare(int row, int col) {
+        String imageName = imageMapping.get(squares[row][col]);
+        if (imageName != null)
+            UI.drawImage(imageName,
+                         leftMargin+(squareSize* col),
+                         topMargin+(squareSize* row),
+                         squareSize, squareSize, false);
+    }
+
+    /** Return true iff the warehouse is solved - 
+     *  all the shelves have boxes on them 
+     */
+    public boolean isSolved() {
+        for(int row = 0; row<rows; row++) {
+            for(int col = 0; col<cols; col++)
+                if(squares[row][col] == Square.shelf)
+                    return  false;
+        }
+        return true;
+    }
+
+    /** Returns the direction that is opposite of the parameter */
+    public String oppositeDirection(String dir) {
+        if ( dir.equals("right")) return "left";
+        if ( dir.equals("left"))  return "right";
+        if ( dir.equals("up"))    return "down";
+        if ( dir.equals("down"))  return "up";
+        return dir;
+    }
+             
+    private void initialiseMappings() {
+        // character in files to square type
+        squareMapping = new HashMap<Character,Square>();
+        squareMapping.put('.', Square.empty);
+        squareMapping.put('A', Square.empty);  // initial position of agent must be an empty square
+        squareMapping.put('#', Square.wall);
+        squareMapping.put('S', Square.shelf);
+        squareMapping.put('B', Square.box);
+
+        // square type to image of square
+        imageMapping = new HashMap<Square, String>();
+        imageMapping.put(Square.empty, "empty.gif");
+        imageMapping.put(Square.wall, "wall.gif");
+        imageMapping.put(Square.box, "box.gif");
+        imageMapping.put(Square.shelf, "shelf.gif");
+        imageMapping.put(Square.boxOnShelf, "boxOnShelf.gif");
+
+        //direction to image of worker
+        agentMapping = new HashMap<String, String>();
+        agentMapping.put("up", "agent-up.gif");
+        agentMapping.put("down", "agent-down.gif");
+        agentMapping.put("left", "agent-left.gif");
+        agentMapping.put("right", "agent-right.gif");
+
+        // key string to direction 
+        keyMapping = new HashMap<String,String>();
+        keyMapping.put("i", "up");     keyMapping.put("I", "up");   
+        keyMapping.put("k", "down");   keyMapping.put("K", "down"); 
+        keyMapping.put("j", "left");   keyMapping.put("J", "left"); 
+        keyMapping.put("l", "right");  keyMapping.put("L", "right");
+
+        keyMapping.put("w", "up");     keyMapping.put("W", "up");   
+        keyMapping.put("s", "down");   keyMapping.put("S", "down"); 
+        keyMapping.put("a", "left");   keyMapping.put("A", "left"); 
+        keyMapping.put("d", "right");  keyMapping.put("D", "right");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        new Sokoban();
+    }
+}
