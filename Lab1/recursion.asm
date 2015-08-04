60badfae8ab498e6f27f5f0cb7464311e8afdd5d
David Barnett <david@barnett.net.nz>
2015-08-05 11:46:47 +1200

    correct stack pointer usage

--- Lab1/recursion.asm
+++ Lab1/recursion.asm
@@ -5,30 +5,30 @@
 
 main:
 
-	
+
 	addi $a0, $zero, 64
 	jal F1 # Jump to F1 & set $ra to here
 	li $2, 10 # exit
 	syscall
 	# F1 takes one parameter in $a0
-	
+
 F1:
 	blt $a0, 1, BaseCase # handle the base case of the recursion
-	
-	# store relevant registers before jump	
+
+	# store relevant registers before jump
+	subi $sp, $sp, 8 # move stack pointer backwards
 	sw $a0, 4($sp)   # store  arg
 	sw $ra, 8($sp)   # store return pointer
-	addi $sp, $sp, 8 # move stack pointer forward
-	
+
 	div $a0, $a0, 2  # set the right parameters for the recursion
 	jal F1           # perform the recursive call
-	
+
 	# reset data after return from recursion
-	subi $sp, $sp, 8 # move stack pointer back
 	lw $a0, 4($sp) # restore arg
 	lw $ra, 8($sp) # restore return pointer
-	
-	
+	addi $sp, $sp, 8 # move stack pointer forward
+
+
 	li $2, 1 # load print
 	syscall  # print parameter already in $a0
 	move $t0, $a0 # copy parameter
@@ -37,6 +37,6 @@ F1:
 	syscall
 	move $a0, $t0 # copy parameter back
 	jr $ra
-	
+
 BaseCase:
 	jr $ra # return to the address given in register $ra
