cab15bc8ae4fd08313a872ddc3ac2cc6c08fcf9c
David Barnett <david@barnett.net.nz>
2016-09-23 21:15:56 +1200

    add comments

--- assign-04/dr.roberts.pl
+++ assign-04/dr.roberts.pl
@@ -27,12 +27,21 @@ writeWord(qm)  :- write("?").
 writeWord(i)  :- write("I"), write(" "). % makes output easier to read 
 writeWord(Word):- write(Word), write(" ").
 
+% takes in a question and resturns an answer
+% 1st argument is the question
+% 2nd argument is the answer
+% This cannot go from an answer to a question, see report.pdf for more
 answer([], []).
 answer(Question, Answer):-
      match(Question, Out, Rest),
      ( Rest = [] -> Partial = [qm] ; answer(Rest, Partial)),
      append(Out, Partial, Answer).
 
+% match a pharse
+% 1st argument is the input array and matches against them
+% 2nd argument is the translated sentance
+% 3rd argument is the rest of the sentance to be matched
+% This cannot go from the outputs to an input
 match([], [], []).
 match(Match, Out, Tail):-
     Match = [M | RestMatch],
@@ -41,9 +50,15 @@ match(Match, Out, Tail):-
         M = you -> ( Out = [your], Tail = RestMatch ) ;
         M = am  -> ( Out = [me], Tail = RestMatch ) ;
         M = my  -> ( Out = [your], Tail = RestMatch ) ;
+        % does not match against anything so just return the word back and move forward a step
         (Out = [M], Tail = RestMatch) 
     ).
 
+% match all the words that start with I
+% 1st argument is the input array and matches against them
+% 2nd argument is the translated sentance
+% 3rd argument is the rest of the sentance to be matched
+% This cannot go from the outputs to an input
 matchI([], [you], []).
 matchI(Match, Out, Tail):-
     Match = [M | Rest],
@@ -52,7 +67,9 @@ matchI(Match, Out, Tail):-
         M = know ->       (Out = [are, you, sure, you, know, that], Tail = Rest) ;
         M = feel ->       (Out = [what, makes, you, feel], Tail = Rest) ;
         M = am   ->       (Out = [you, are], Tail = Rest) ;
+        % has the extra step of adding the "before" to the end of the sentance so it reads slightly better
         M = fantasised -> (Out = [have, you, ever, fantasised], append(Rest, [before], Tail)) ;
+        % does not match against anything so just return the word back and move forward a step
         (Out = [you], Tail = Match) 
     ).
 
@@ -69,7 +86,6 @@ printReply(Question):- answer(Question, Answer), printSentance(Answer).
 :- answer([i, fantasised, about, fast, cars], [have, you, ever, fantasised, about, fast, cars, before, qm]).
 :- answer([i, feel, bad, about, my, brother], [what, makes, you, feel, bad, about, your, brother, qm]). 
 :- answer([i, know, i, am, insecure], [are, you, sure, you, know, that, you, are, insecure, qm]).
-
 :- answer([i, like, my, mother], [why, you, like, your, mother, qm]).
 
 % vim: set sw=4 ts=4 expandtab ft=prolog: 
