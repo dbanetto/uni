2100ae8cec82baa9193e0ba2004ab369705dd409
David Barnett <david@barnett.net.nz>
2017-03-29 12:00:43 +1300

    implement the execerise

--- exercise/src/exercise.adb
+++ exercise/src/exercise.adb
@@ -1,58 +1,76 @@
+with Ada.Text_IO;
 with Ada.Assertions; use Ada.Assertions;
 
 package body Exercise with Spark_Mode is
+
    function InArray (value : Integer; input : IntArray) return Boolean is
    begin
       for i in input'Range loop
-         if input(i) = value then
+         if input (i) = value then
             return True;
          end if;
       end loop;
       return False;
    end InArray;
 
-   procedure DeleteValue
-     (value  :        Integer;
-      input  : in     IntArray;
-      output :    out IntArray) is
-
-      index : Integer;
-      count : Integer := 0;
+   function DeleteValue
+     (value :    Integer;
+      input : in IntArray) return IntArray is
    begin
-
       for i in input'Range loop
          if input(i) = value then
-            if count = Integer'Last then
-               exit;
-            end if;
-            count := count + 1;
+            return input(input'First .. i - 1) & DeleteValue(value, input(i + 1 .. input'Last));
          end if;
       end loop;
 
-      Assert( count <= input'Length );
-
-      output := input(input'First .. (input'Last - count));
-      index := output'First;
-
-      if output'Length = 0 then
-         return;
-      end if;
-
-      for i in input'Range loop
-         pragma Loop_Invariant (index in output'Range);
-
-         if input(i) /= value then
-            output(index) := input(i);
-            index := Integer'Succ(index);
-         end if;
-      end loop;
+      return input;
    end DeleteValue;
 
-
-
-   function ContcatArray ( a : IntArray ; b : IntArray) return IntArray is
-      output : IntArray := a & b;
+   function ContcatArray
+     (a : OrderedIntArray;
+      b : OrderedIntArray) return OrderedIntArray
+   is
+      output : OrderedIntArray := a & b;
    begin
       return output;
    end ContcatArray;
+
+   function MergeArray
+     (a : OrderedIntArray;
+      b : OrderedIntArray) return OrderedIntArray
+   is
+      aIndex : Integer  := a'First;
+      bIndex : Integer  := b'First;
+      c      : IntArray := IntArray (a) & IntArray (b);
+      cIndex : Integer  := c'First;
+   begin
+
+      loop
+         exit when aIndex > a'Last and bIndex > b'Last;
+
+         if aIndex > a'Last then
+            c (cIndex) := b (bIndex);
+            cIndex     := cIndex + 1;
+            bIndex     := bIndex + 1;
+         elsif bIndex > b'Last then
+            c (cIndex) := a (aIndex);
+            cIndex     := cIndex + 1;
+            aIndex     := aIndex + 1;
+         else
+            -- need to compare
+            if a (aIndex) <= b (bIndex) then
+               c (cIndex) := a (aIndex);
+               cIndex     := cIndex + 1;
+               aIndex     := aIndex + 1;
+            else
+               c (cIndex) := b (bIndex);
+               cIndex     := cIndex + 1;
+               bIndex     := bIndex + 1;
+            end if;
+         end if;
+      end loop;
+
+      return OrderedIntArray (c);
+   end MergeArray;
+
 end Exercise;
--- exercise/src/exercise.ads
+++ exercise/src/exercise.ads
@@ -3,30 +3,25 @@ package Exercise with
 
    type IntArray is array (Integer range <>) of Integer;
 
+   subtype OrderedIntArray is IntArray
+     with Predicate => (for all i in OrderedIntArray'First .. OrderedIntArray'Last -1
+                        => OrderedIntArray(i) <= OrderedIntArray(i + 1));
+
    function InArray (value : Integer; input : IntArray) return Boolean with
       Post => InArray'Result =
       (for some k in input'Range => value = input (k));
 
-   procedure DeleteValue
+   function DeleteValue
      (value  :        Integer;
-      input  : in     IntArray;
-      output :    out IntArray) with
-      Pre => input'Length >= 1,
-     Post => input'Length >= output'Length and
-             (for all i in output'Range => output (i) /= value);
+      input  : in     IntArray) return IntArray with
+     Post => (for all i in DeleteValue'Result'Range => DeleteValue'Result(i) /= value);
 
-   function ContcatArray (a : IntArray; b : IntArray) return IntArray with
+   function ContcatArray (a : OrderedIntArray; b : OrderedIntArray) return OrderedIntArray with
       Pre =>
-       (a'Length >= 1 and b'Length >= 1 and
-        (for all i in a'First .. a'Last - 1 => a (i) <= a (i + 1)) and
-        (for all i in b'First .. b'Last - 1 => b (i) <= b (i + 1)) and
-        a (a'Last) <= b (b'First)
+       (a (a'Last) <= b (b'First)),
+      Post => ContcatArray'Result'Length = a'Length + b'Length;
 
-      ),
-
-      Post => ContcatArray'Result'Length = a'Length + b'Length and
-      (for all i in
-         ContcatArray'Result'First .. ContcatArray'Result'Last - 1 =>
-         ContcatArray'Result (i) <= ContcatArray'Result (i + 1));
+   function MergeArray (a : OrderedIntArray; b : OrderedIntArray) return OrderedIntArray with
+      Post => MergeArray'Result'Length = a'Length + b'Length;
 
 end Exercise;
