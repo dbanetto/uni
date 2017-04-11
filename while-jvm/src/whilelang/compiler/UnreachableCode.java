77876913cf658c05af6e0b608d7b07e3bd591a3c
David Barnett <david@barnett.net.nz>
2017-04-11 13:49:35 +1200

    start of assignment 2

---
+++ while-jvm/src/whilelang/compiler/UnreachableCode.java
@@ -0,0 +1,27 @@
+// This file is part of the WhileLang Compiler (wlc).
+//
+// The WhileLang Compiler is free software; you can redistribute
+// it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// The WhileLang Compiler is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with the WhileLang Compiler. If not, see
+// <http://www.gnu.org/licenses/>
+//
+// Copyright 2013, David James Pearce.
+
+package whilelang.compiler;
+
+import whilelang.ast.WhileFile;
+
+public class UnreachableCode {
+	public void check(WhileFile wf) {
+		
+	}
+}
