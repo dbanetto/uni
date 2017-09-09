41c2d38fae1fec54660b056b0978e86e89f506bd
David Barnett <david@barnett.net.nz>
2017-09-10 11:22:56 +1200

    Compete part 2's code

--- assign3/sql/q2_udf.sql
+++ assign3/sql/q2_udf.sql
@@ -1,16 +1,40 @@
 CREATE OR REPLACE FUNCTION coursePass(sid integer, cid unknown, year integer, grade unknown, grad unknown)
-RETURNS record AS $$
+RETURNS boolean AS $$
+DECLARE
+    ssid ALIAS FOR sid;
+    ccid ALIAS FOR cid;
+    totalpts RECORD; 
+    gradt TEXT;
+    gradd DATE;
 BEGIN
-    grad := (grad::text)::Date;
+    gradt := grad::text;
+    gradd := gradt::Date;
     cid := cid::varchar(7);
     grade := grade::varchar(2);
+
     -- A student can get more than one grade for the same course, but each in a different year.
+    INSERT INTO result VALUES(sid, cid, year, grade); -- The PK should prevent double ups
 
     -- When a student passes a course, she/he will have the points for this course added to
     -- her/his earned points. A student may earn the points for the same course only once.
-
+    SELECT SUM(points) AS points INTO totalpts FROM (SELECT MIN(points) AS points FROM result NATURAL JOIN course WHERE result.sid = ssid group by (result.cid, result.sid)) as p;
+    UPDATE student SET pointsEarned = (totalpts.points) WHERE student.sid = ssid;
+    
     -- When a student has earned 360 points or more, she/he is recorded in the GRADUATE table.
+    IF totalpts.points >= 360 THEN
+        PERFORM * FROM graduate WHERE graduate.sid = ssid;
+        IF FOUND THEN
+            UPDATE graduate SET graduationdate = gradd WHERE graduate.sid = ssid;
+        ELSE 
+            INSERT INTO Graduate (sid, graduationdate) VALUES (ssid, gradd);
+        END IF;
+    ELSE 
+        PERFORM * FROM graduate WHERE graduate.sid = ssid;
+        IF FOUND THEN
+            DELETE FROM graduate WHERE graduate.sid = ssid;
+        END IF;
+    END IF;
 
-    return (sid, cid, year, grade, grad);
+    return true;
 END;
 $$ LANGUAGE 'plpgsql';
